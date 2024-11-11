efcb117ffd40518d845984aeb371d932ad50e598
leebaumgartner <leebaumgartner03@gmail.com>
2024-11-10 19:56:19 -0600

    Add files via upload

---
+++ LBaumgartner- Lesson 2 Assignment.py
@@ -0,0 +1,8 @@
+print ("Hello! Welcome to the age calculator, let me ask you some questions!")
+first_name = input ("What is your first name?")
+last_name = input ("What is your last name?")
+current_year = int(input ("What year is it?"))
+birth_year = int(input ("What year were you born?"))
+age = current_year-birth_year
+print ("Hello", first_name + " " + last_name) print ("You are", age ,"years old!" ), print ("In the next year", current_year+1, "you will be", age+1, "years old!")
+print ("Completed by Lee Baumgartner")
